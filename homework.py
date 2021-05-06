e2fc7acdb412b941820dd43325a69c6541ae29ef
Andrey_Ivantsiv <andrey.ivantsiv@gmail.com>
2021-05-06 23:23:19 +1100

    rev1

--- homework.py
+++ homework.py
@@ -11,11 +11,15 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+url_api = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+bot_сlient = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    """Определяет статус домашней работы."""
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -23,14 +27,15 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    """Получает статус домашней работы."""
+    data = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url_api, params=data, headers=headers)
     return homework_statuses.json()
 
 
-def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+def send_message(message, bot_сlient):
+    """Отправляет сообщение в телеграмм."""
+    return bot_сlient.send_message(CHAT_ID, message)
 
 
 def main():
@@ -41,8 +46,10 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]), bot_сlient)
+            current_timestamp = new_homework.get('current_date',
+                                                 current_timestamp)
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
