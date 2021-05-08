a9998e1061f975e79c292959c36009fed23aa0b2
Andrey_Ivantsiv <andrey.ivantsiv@gmail.com>
2021-05-08 23:29:53 +1100

    rev2

--- homework.py
+++ homework.py
@@ -1,45 +1,77 @@
+import logging
 import os
 import time
+from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
 from dotenv import load_dotenv
 
-load_dotenv()
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[RotatingFileHandler('log/main.log', maxBytes=50000000,
+                                  backupCount=5, encoding='utf-8')],
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
+formatter = logging.Formatter(
+    '%(asctime)s, %(levelname)s, %(message)s, %(name)s')
+
+logger_bot = logging.getLogger(__name__)
+logger_bot.setLevel(logging.DEBUG)
+handler_bot = RotatingFileHandler('log/logger_bot.log', maxBytes=10000000,
+                                  backupCount=5, encoding='utf-8')
+handler_bot.setFormatter(formatter)
+logger_bot.addHandler(handler_bot)
+
+load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 url_api = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-bot_сlient = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
     """Определяет статус домашней работы."""
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    if homework['homework_name'] and homework['status']:
+        homework_name = homework['homework_name']
+        verdict = ('Работа не НАЙДЕНА или Статус работы неизвестен, '
+                   'проверте правильность запроса!')
+        if homework['status'] == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        elif homework['status'] == 'approved':
+            verdict = ('Ревьюеру всё понравилось, '
+                       'можно приступать к следующему уроку.')
+        elif homework['status'] == 'reviewing':
+            verdict = 'Работа проверяется.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return verdict
 
 
 def get_homework_statuses(current_timestamp):
     """Получает статус домашней работы."""
     data = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url_api, params=data, headers=headers)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(url_api, params=data, headers=headers)
+        return homework_statuses.json()
+    except requests.exceptions.RequestException as er:
+        logger_bot.error(f'Ошибка {er} при запросе статуса домашней работы')
 
 
 def send_message(message, bot_сlient):
     """Отправляет сообщение в телеграмм."""
+    logger_bot.info('Отправка сообщения')
     return bot_сlient.send_message(CHAT_ID, message)
 
 
 def main():
     # проинициализировать бота здесь
+    bot_сlient = telegram.Bot(token=TELEGRAM_TOKEN)
+    logger_bot.debug('Запуск бота')
+    send_message('Бот запущен!', bot_сlient)
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
@@ -50,11 +82,11 @@ def main():
                     new_homework.get('homeworks')[0]), bot_сlient)
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
-            time.sleep(300)  # опрашивать раз в пять минут
-
+            time.sleep(300)
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            logger_bot.error(f'Бот столкнулся с ошибкой: {e}')
+            send_message(f'Бот столкнулся с ошибкой: {e}', bot_сlient)
+            time.sleep(150)
 
 
 if __name__ == '__main__':
